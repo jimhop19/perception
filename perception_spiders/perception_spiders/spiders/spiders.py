02d34e771a202c3d7af2126386a2b08fb7c3f858
Jimmy <jimmy@Jimmyde-MacBook-Pro.local>
2023-11-13 12:25:43 +0800

    up to 5 crawlers

--- perception_spiders/perception_spiders/spiders/spiders.py
+++ perception_spiders/perception_spiders/spiders/spiders.py
@@ -1,75 +1,164 @@
-from typing import Iterable
 import scrapy
-import json
-from scrapy.crawler import CrawlerProcess
-from scrapy.http import Request
-from scrapy.utils.project import get_project_settings
-import multiprocessing
 
 class SpiderPtsSpider(scrapy.Spider):
     name = "spider_pts"
     allowed_domains = ["news.pts.org.tw"]
-    # start_urls = ["https://news.pts.org.tw/search/藍白"]
-
-    # def __init__(self, *args, **kwargs):
-    #     super(SpiderPtsSpider, self).__init__(*args, **kwargs)        
-    #     # self.start_urls = f'https://news.pts.org.tw/search/{keyword}'        
-    #     self.results = []
+    
     def start_requests(self):
-        yield scrapy.Request(f'https://news.pts.org.tw/search/{self.keyword}')
-        # self.results = []
+        yield scrapy.Request(f'https://news.pts.org.tw/search/{self.keyword}')        
 
     def parse(self, response, **kwargs):
         articles = response.xpath("//ul[@class='list-unstyled search-list relative-news-list-content']/li")
 
-        # count = 0
+        count = 0
         for article in articles:
             title = article.xpath(".//div/h2/a/text()").get()
-            yield {
-                "name":title
-            }
-            # title = article.xpath(".//div/h2/a/text()").get()
-            # self.results.append({'title': title})
-            # count += 1
-            # if count == 5:
-            #     break
+            link = article.xpath(".//figure/a/@href").get()
+            photo = article.xpath(".//figure/a/div/div/img/@src").get()
+            time = article.xpath(".//div/div/time/text()").get().replace(" ","").replace("\n","")
+            category = article.xpath(".//div/div/a/text()").get()
+            content = article.xpath(".//div/p/text()").get().replace(" ","").replace("\n","")
 
-    # def closed(self, reason):
-    #     # Write the results to a JSON file
-    #     print (self.results)
-    #     return self.results
-    #     # with open('pts.json', 'w', encoding='utf-8') as file:
-    #     #     json.dump(self.results, file, ensure_ascii=False, indent=4)
-    #     # return self.results
+            yield {
+                "title":title,
+                "link":link,
+                "photo":photo,
+                "time":time,
+                "category":category,
+                "content":content
+            }
+            count += 1
+            if count == 5:
+                break    
 
 
 class SpiderUdnSpider(scrapy.Spider):
     name = "spider_udn"
     allowed_domains = ["udn.com"]
-    start_urls = ["https://udn.com/search/word/2/藍白"]
-    def __init__(self, *args, **kwargs):
-        super(SpiderUdnSpider, self).__init__(*args, **kwargs)
-        # self.start_urls = [f"https://udn.com/search/word/2/{keyword}"]
-        self.results = []
 
-    def parse(self, response):
-        articles = response.xpath("//div[@class='story-list__news']/div[2]")
-        count = 0
-        for article in articles:
-            title = article.xpath(".//h2/a/text()").get()
-            self.results.append({'title': title})
+    def start_requests(self):
+        yield scrapy.Request(f'https://udn.com/search/word/2/柯')
+        print("aaaa")
+    
+    def parse(self, response, **kwargs):  
+        print("bbbb")              
+        articles = response.xpath("//div[@class='story-list__news']")
+        count = 0    
+        print("cccc")
+        print(articles)
+        for article in articles:            
+            photo = article.xpath(".//div[1]/a/picture/img/@src").get()
+            link = article.xpath(".//div[1]/a/@href").get()
+            title = article.xpath(".//div[2]/h2/a/text()").get()
+            content = article.xpath(".//div[2]/p/text()").get()
+            time = article.xpath(".//div[2]/div/time/text()").get()
+            category = article.xpath(".//div[2]/div/a/text()").get()
+            print("dddd")
+            yield {
+                "title":title,
+                "link":link,
+                "photo":photo,
+                "time":time,
+                "category":category,
+                "content":content
+            }
+
             count += 1
             if count == 5:
                 break
 
-    def closed(self, reason):        
-        # Write the results to a JSON file
-        print (self.results)
-        # with open('udn.json', 'w', encoding='utf-8') as file:
-        #     json.dump(self.results, file, ensure_ascii=False, indent=4)
+class SpiderLtnSpider(scrapy.Spider):
+    name = "spider_ltn"
+    allowed_domains = ["search.ltn.com.tw","news.ltn.com.tw"]
+    
+    def start_requests(self):
+        yield scrapy.Request(f'https://search.ltn.com.tw/list?keyword={self.keyword}')        
 
-# settings = get_project_settings()
-# process = CrawlerProcess(settings)
-# process.crawl(SpiderPtsSpider)
-# process.crawl(SpiderUdnSpider)
-# process.start()
+    def parse(self, response, **kwargs):
+        articles = response.xpath("//ul[@class='list boxTitle']/li")
+        count = 0
+        for article in articles:
+            photo = article.xpath(".//a/img/@data-src").get()
+            link = article.xpath(".//a/@href").get()
+            category = article.xpath(".//div/i/text()").get()          
+            title = article.xpath(".//div/a/text()").get()
+            content = article.xpath(".//div/p/text()").get()                        
+            
+            item = {
+                    "title": title,
+                    "link": link,
+                    "photo": photo,                
+                    "category": category,
+                    "content": content,
+                }   
+            yield scrapy.Request(url=link,callback=self.parse_time,cb_kwargs={"item":item})   
+    
+            count += 1
+            if count == 5:
+                break
+
+    def parse_time(self,response,item):
+        item_add_time = item
+        item_add_time["time"] =  response.xpath("//span[@class='time']/text()").get()
+        yield item_add_time
+
+class SpiderEttodaySpider(scrapy.Spider):
+    name = "spider_ettoday"
+    allowed_domains = ["www.ettoday.net"]
+    
+    def start_requests(self):   
+        yield scrapy.Request(f"https://www.ettoday.net/news_search/doSearch.php?keywords={self.keyword}&idx=2/")
+
+    def parse(self, response, **kwargs):        
+        articles = response.xpath("//div[@class='archive clearfix']")
+        count = 0        
+        for article in articles:
+            photo = article.xpath(".//div[1]/a/img/@src").get()
+            link = article.xpath(".//div[1]/a/@href").get()
+            category = article.xpath(".//div[2]/p/span[@class='date']/a/text()").get()          
+            title = article.xpath(".//div[2]/h2/a/text()").get()
+            content = article.xpath(".//div[2]/p/text()").get()
+            time = article.xpath(".//div[2]/p/span[@class='date']//text()").getall()[2].replace("/","").replace(")","")
+            
+            item = {
+                    "title": title,
+                    "link": link,
+                    "photo": photo,                
+                    "category": category,
+                    "content": content,
+                    "time":time
+                }   
+            yield item
+    
+            count += 1
+            if count == 5:
+                break
+
+class SpiderCnaSpider(scrapy.Spider):
+    name = "spider_cna"
+    allowed_domains = ["cna.com.tw"]
+    
+    def start_requests(self):
+        yield scrapy.Request(f'https://www.cna.com.tw/search/hysearchws.aspx?q={self.keyword}')        
+
+    def parse(self, response, **kwargs):
+        articles = response.xpath("//ul[@class='mainList']/li")
+        count = 0
+        for article in articles:
+            link = article.xpath(".//a/@href").get()
+            item = {}
+            yield scrapy.Request(url=link,callback=self.parse_category_and_content,cb_kwargs={"item":item})   
+    
+            count += 1
+            if count == 5:
+                break
+    def parse_category_and_content(self,response,item):
+        item_add = item
+        item_add["category"] = response.xpath("//a[@class='blue']/text()").get()
+        item_add["content"] = response.xpath("//div[@class='paragraph']/p[1]/text()").get()
+        item_add["title"] = response.xpath("//h1/span/text()").get()
+        item_add["time"] = response.xpath("//div[@class='timeBox']/div/span[1]/text()").get()
+        item_add["link"] = response.url
+        item_add["photo"] = response.xpath(".//div[@class='fullPic']/figure/picture/source/@srcset").get()
+        yield item_add
+
